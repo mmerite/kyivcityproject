74e0fe56f15e17e9c592761e4e898749a1e0c769
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-11 20:53:15 +0000

    centered footer

--- components/shared/footer/index.js
+++ components/shared/footer/index.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Footer({href, title}) {
     return (
-    <div classname="bg-indigo-500 w-full max-w-6xl mx-auto py-4 px-4">
+    <div className="w-full max-w-6xl mx-auto mt-8">
         <Link href={href}>
-            Go to {title}
+            {title}
         </Link>
     </div>
     );

04e4529719409ddee7b0921f4a3505456fb7723d
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-12 13:47:02 +0000

    corrected bio

---
+++ components/home/hero/bio/index.js
@@ -0,0 +1,23 @@
+export default function Bio() {
+    return (
+        <div className="w-full max-w-6xl mx-auto mt-8">
+             <p className="indent-6 font-light text-gray-500 whitespace-pre-line"> I lived and worked in Kyiv, Ukraine, until April 2022. Before the war I saw the world with the amusement of a child, and it fascinated me to a such extent that I could not help materializing it in a some form, 
+             either visual (watercolouring and graphic drawing), or physical (dancing), or audible (singing).{'\n'} {'\n'}
+            </p>
+
+            <p className="indent-6 font-light text-gray-500 whitespace-pre-line"> 
+                My main visual media has always been watercolour, since I loved the water itself and enjoyed its spontaneity.
+                In a working process, I considered it an equal partnership between myself, the water, the paper and the paint, and I liked to watch how the image was born out of it.
+                Basically, all my art was the way of exploration and self-study, through finding a harmony between what I saw outside, what emerged on paper and what was inside of me at the moment of creation. {'\n'} {'\n'}
+            </p>
+
+             <p className="indent-6 font-light text-gray-500 whitespace-pre-line"> I was part of Urban Sketchers Ukraine group and was deeply in love with outdour painting. It was the best way to capture the mood of a day and the atmosphere of a place. {'\n'} {'\n'}
+             </p>
+
+             <p className="indent-6 font-light text-gray-500 whitespace-pre-line">I loved my home city. This is how my Kyiv sketches project was born.{'\n'} {'\n'}
+             </p>
+
+            
+        </div>
+    );
+}
---
+++ components/home/hero/index.js
@@ -0,0 +1,22 @@
+import Bio from "./bio";
+
+export default function Hero({name, subtitle, imgUrl}) {
+    return (
+        <div className="w-full max-w-6xl mx-auto mt-8">
+            <div className="w-full flex flex-row-2 space-x-24 py-4">
+            
+                <div className="w-4/12">
+                    <img src={imgUrl} className="rounded-lg"></img>
+                </div>
+
+                <div className="w-6/12">
+                    <p className="indent-6 text-xl text-gray-700">{subtitle}</p>
+                    
+                    <Bio className="text-left"></Bio>
+                </div>
+
+
+            </div>
+        </div>
+    );
+}

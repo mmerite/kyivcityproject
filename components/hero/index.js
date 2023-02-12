68d15c3255d8369e4f4ad6557b3cfb44605fa5be
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-12 12:00:49 +0000

    added bio to hero component

--- components/hero/index.js
+++ components/hero/index.js
@@ -1,4 +1,6 @@
-export default function Hero({name, bio, imgUrl}) {
+import Bio from "./bio";
+
+export default function Hero({name, subtitle, imgUrl}) {
     return (
         <div className="w-full max-w-6xl mx-auto mt-8">
             <div className="w-full flex flex-row space-x-2 py-4">
@@ -7,8 +9,12 @@ export default function Hero({name, bio, imgUrl}) {
                     <img src={imgUrl} className="h-32"></img>
                 </div>
 
-                <div className="w-110/12">
-                    <p className="text-xl text-gray-700">{bio}</p>
+                <div className="w-10/12">
+                    <p className="text-xl text-gray-700">{subtitle}</p>
+                </div>
+
+                <div className="w-10/12">
+                    <Bio></Bio>
                 </div>
 
             </div>

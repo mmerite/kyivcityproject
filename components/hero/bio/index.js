51cec9c966cc7df21cd25daf7e83619654925b63
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-12 12:01:38 +0000

    added bio to hero component

---
+++ components/hero/bio/index.js
@@ -0,0 +1,17 @@
+export default function Bio() {
+    return (
+        <div className="w-full max-w-6xl mx-auto mt-8">
+             <p className="indent-6 text-justify font-light text-gray-500 whitespace-pre-line"> Maria Mykytiuk - Merite Watercolour - born in 1982, who lived and worked in Kyiv, Ukraine until April 2022.{'\n'}{'\n'}
+            </p>
+        
+             <p className="indent-6 text-justify font-light text-gray-500 whitespace-pre-line"> I see the world with the amusement of a child, and it fascinates me to a such extent that I cannot help materializing it in a some form, 
+             either visual (watercolouring and graphic drawing), or physical (dancing), or audible (singing).
+             </p>
+
+             <p className="indent-6 text-justify font-light text-gray-500 whitespace-pre-line"> 
+             My current main visual media is watercolour, since I love the water itself and enjoy its spontaneity.
+             In a working process, I consider it an equal partnership between myself, the water, the paper and the paint, and I like to watch how the image is born out of it.
+             Basically, all my art is the way of exploration and self-study, through finding a harmony between what I see outside, what emerges on paper and what is inside of me at the moment of creation. </p>
+        </div>
+    );
+}

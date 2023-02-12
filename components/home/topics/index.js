68cf8ab22f1ca0c243eaaf77030262ea3b2dacc5
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-12 13:58:02 +0000

    added button see sketches to topic component

--- components/home/topics/index.js
+++ components/home/topics/index.js
@@ -1,6 +1,6 @@
 import topics from "./topics"
 
-export default function Topics(imgUrl) {
+export default function Topics(imgUrl, href) {
     return (
         <div className="w-full flex flex-col-2">
             <div className="w-5/12 flex flex-col py-4 max-w-sm" >
@@ -13,6 +13,13 @@ export default function Topics(imgUrl) {
                         return <li key={index}> {topic} </li>
                     })}
                 </div>
+                <button 
+                    className="bg-orange-300 hover:bg-orange-400 text-white py-2 px-4 rounded-lg w-40 mx-auto my-8"
+                    type="button">
+                    <a href="/sketches"> See sketches </a>
+                </button>
+
+                
              </div>
 
             <div className="w-7/12 py-4 px-8 mx-auto">

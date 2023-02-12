90121fd0e6d9ae0b256bf22ac06cd6060f42009a
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-12 14:35:49 +0000

    added like button to sketch-item page

--- components/sketch-grid/sketch-item.js
+++ components/sketch-grid/sketch-item.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 export default function SketchItem({sketch}) {
+    const [likes, setLikes] = useState(0);
+
+    const handleLike = () => {
+        let newLikes = likes + 1;
+        setLikes(newLikes);
+    }
+
     return (
         <div className="border rounded-lg p-4">
             <img src={sketch.imgUrl}className=""></img>
             <p className="text-md font-Georgia font-medium">{sketch.title}</p>
             <p className="font-light text-sm text-gray-700">{sketch.medium}</p>
             <p className="font-light italic text-sm text-gray-700">{sketch.size}</p>
+            
+            <button 
+            className="rounded px-2 py-1 bg-indigo-400 text-white"
+            type="button"
+            onClick={handleLike}>
+                Love ({likes})
 
+            </button>
         </div>
     );
 }

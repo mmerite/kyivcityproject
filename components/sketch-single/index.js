f5e5bdba9128244327b2601d45f430fe988a2a27
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-13 23:25:32 +0000

    added Title to single sketch layout component

--- components/sketch-single/index.js
+++ components/sketch-single/index.js
@@ -1,16 +1,17 @@
-export default function SketchSingle({imgUrl, medium, size, description}) {
+export default function SketchSingle({imgUrl, title, medium, size, description}) {
     return (
         <div className="w-full max-w-6xl mx-auto mt-8">
-            <div className="w-full flex flex-row-2 space-x-24 py-4">
+            <div className="w-full flex flex-cols-2 space-x-24 py-4">
             
-                <div className="w-6/12">
+                <div className="w-7/12 py-4">
                     <img src={imgUrl} className="rounded-lg"></img>
                 </div>
 
-                <div className="w-6/12">
-                    <p className="text-light text-gray-500">Medium: {medium}</p>
-                    <p className="text-light text-gray-500"> Size: {size}</p>
-                    <p className="text-light text-gray-500">Story about: {description}</p>
+                <div className="w-5/12 py-4 mt-8">
+                    <h2 className="font-bold py-2"> "{title}"</h2>
+                    <p className="text-light text-gray-500 py-2">Medium: {medium}</p>
+                    <p className="text-light text-gray-500 py-2"> Size: {size}</p>
+                    <p className="text-light text-gray-500 py-2">Story about: {description}</p>
                     
                 </div>
 

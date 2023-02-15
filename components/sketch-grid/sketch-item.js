9255ae5a49ad1a2910ae6faec756aa94c931bfa1
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-15 18:05:09 +0000

    made clickable image & title for single page

--- components/sketch-grid/sketch-item.js
+++ components/sketch-grid/sketch-item.js
@@ -11,9 +11,10 @@ export default function SketchItem({sketch}) {
 
     return (
         <div className="border rounded-lg p-4">
-            <img src={sketch.img_url} className="rounded"></img>
+            
             <Link href={`/single-sketch/${sketch.id}`} className="text-md font-medium">
-                {sketch.title}</Link>
+               <span><img src={sketch.img_url} className="rounded"></img> {sketch.title} </span>
+               </Link>
             <p className="font-light text-sm text-gray-700">{sketch.medium}</p>
             <p className="font-light italic text-sm text-gray-700">{sketch.size}</p>
             

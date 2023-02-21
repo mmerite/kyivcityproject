00fd8e3c364e68502326239379d39500d77e0431
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-21 21:02:44 +0000

    resized image and top margin for single sketch

--- components/sketch-single/index.js
+++ components/sketch-single/index.js
@@ -4,10 +4,10 @@ export default function SketchSingle({imgUrl, title, medium, size, description})
             <div className="w-full flex flex-cols-2 space-x-24 py-4">
             
                 <div className="w-7/12 py-4">
-                    <img src={imgUrl} className="rounded-lg"></img>
+                    <img src={imgUrl} className="object-contain aspect-[4/4] rounded-lg"></img>
                 </div>
 
-                <div className="w-5/12 py-4 mt-8">
+                <div className="w-5/12 py-4 mt-28">
                     <h2 className="font-bold py-2"> "{title}"</h2>
                     <p className="text-light text-gray-500 py-2">Medium: {medium}</p>
                     <p className="text-light text-gray-500 py-2"> Size: {size}</p>

25edc55567877ecea53937c3286c2f721133de96
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-22 21:14:48 +0000

    removed px-4 menu button component

--- components/shared/navigation-bar/menu-button.js
+++ components/shared/navigation-bar/menu-button.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function MenuButton({href, title, children}) {
     return (
-        <div className="px-4">
+        <div className="">
             <button 
                 className="hover:bg-orange-400 text-white py-2 px-4 rounded-lg"
                 type="button">

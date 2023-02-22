757e3a060b25497b2032446c5d36c94c7043b688
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-22 21:06:33 +0000

    pages updated no changes

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Topics from "@/components/home/topics";
 import Hero from "@/components/home/hero";
 import Content from "@/components/shared/Content";
-import Footer from "@/components/shared/footer";
 import Header from "@/components/shared/header";
 
 export default function Home() {
   return (
     <div className="w-full"> 
 
-    <Header name ="MeriteWatercolour"/>
+    <Header name ="MeriteWatercolour"
+  
+    />
 
     <Hero
       imgUrl="/assets/profile_1.jpeg" 
--- pages/single-sketch/[sketchId].js
+++ pages/single-sketch/[sketchId].js
@@ -1,8 +1,11 @@
 import Header from "@/components/shared/header";
 import Footer from "@/components/shared/footer";
+import Content from "@/components/shared/Content";
 import { useRouter } from "next/router"
 import { useEffect,useState } from "react";
 import SketchSingle from "@/components/sketch-single";
+import Button from "@/components/shared/Buttons/button";
+import MenuButton from "@/components/shared/navigation-bar/menu-button";
 
 export default function SingleSketch(){
     const router = useRouter();
@@ -27,13 +30,19 @@ export default function SingleSketch(){
 
     if (!sketch) {
         return (
-            <div> Loading...</div>
-        )
+            <div>Loading...</div>
+            
+        );
     }
+
 
     return (
         <div className="w-full">
-            <Header name={sketch.title} />
+            
+                <Header name={sketch.title}>
+                    <MenuButton title="Home" href="/" className="bg-color-indigo-400"></MenuButton>
+                </Header>    
+
             <SketchSingle
                 imgUrl={sketch.img_url}
                 title={sketch.title}
@@ -43,25 +52,29 @@ export default function SingleSketch(){
             />
 
 
+            <div className="w-full"> 
+                    <Footer> 
+                        <div className="flex flex-row space-x-8"> 
+                            <Button 
+                                href={`/single-sketch/${+sketchId - 1}`}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 8 8 12 12 16"></polyline><line x1="16" y1="12" x2="8" y2="12"></line></svg>
+                            </Button>
 
-            <div className="w-full flex flex-row"> 
-                <Footer href="/" title="Return Home"/>
-                
-                <div className ="flex flex-row space-x-1">
-                    <Footer className=""
-                    title="Previous sketch"
-                    href={`/single-sketch/${+sketchId - 1}`}
-                    />
+                            <Button 
+                                title="Back to sketches"
+                                href="/sketches">
+                            </Button>
 
-                    <Footer className=""
-                    title="Next sketch"
-                    href={`/single-sketch/${+sketchId + 1}`}
-                    />
-                </div>
+                            <Button 
+                                href={`/single-sketch/${+sketchId + 1}`}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+                            </Button>
 
-                <Footer href="/sketches" title="Return to sketches"/>
-               
+                        </div>
+
+                    </Footer>
             </div>
+               
         </div>
     )
 }
@@ -69,3 +82,23 @@ export default function SingleSketch(){
 
 ///console.log("router", sketchId); - this is to check if it goes through ok
 //console.log("router", router.query.sketchId)
+
+// to be finished below
+    /*if (sketchId >= 9) {
+            return (<div className="w-full"> 
+            <Header name="MeriteWatercolour">
+                <MenuButton title="Home" href="/" className="bg-color-indigo-400"></MenuButton>
+            </Header>
+
+            <Content>
+                <div className="mt-16"> 
+                So sorry the page is not found!
+                </div>
+                <div className="mt-8">
+                    <Button href="/sketches" title="Back to sketches"></Button>
+                </div>
+            </Content>
+
+            </div>
+            );
+        } */

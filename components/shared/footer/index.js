05fbefe3cf8d5ff6c116e57cdf196580feedfab0
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-22 19:59:19 +0000

    passed {children} inside footer and header

--- components/shared/footer/index.js
+++ components/shared/footer/index.js
@@ -1,16 +1,9 @@
 import Link from "next/link";
 
-export default function Footer({href, title}) {
+export default function Footer({children}) {
     return (
-    <div className="w-full flex flex-fow justify-center space-x-6 mx-auto mt-8" role="group">
-       
-       <button 
-            className="bg-orange-300 hover:bg-orange-400 text-white py-2 px-4 rounded-lg"
-            type="button">
-                <Link href={href}>
-                    {title}
-                </Link>
-         </button>
+    <div className="w-full flex flex-fow justify-center space-x-6 mx-auto mt-12">
+       {children}
     </div>
     );
 }
--- components/shared/header/index.js
+++ components/shared/header/index.js
@@ -1,7 +1,10 @@
-export default function Header({name}) {
+import Link from "next/link";
+export default function Header({name, children}) {
     return (
-        <div className="bg-indigo-500 w-full max-w-6xl mx-auto py-4 px-4">
-            <h1 className="font-medium text-3xl text-white">{name}</h1>
+        <div className="bg-indigo-500 w-full flex flex-row justify-between max-w-6xl mx-auto py-4 px-4">
+            <h1 className="w-3/4 font-medium text-3xl text-white">{name}</h1>
+            {children}
         </div>   
         );
 }
+

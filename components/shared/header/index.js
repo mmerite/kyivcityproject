24b55e3adfd83ad7d3758e0ec0648b89c6fafe32
“mmerite” <mar.mykytiuk@gmail.com>
2023-02-22 21:33:52 +0000

    updated header layout

--- components/shared/header/index.js
+++ components/shared/header/index.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 export default function Header({name, children}) {
     return (
-        <div className="bg-indigo-500 w-full flex flex-row justify-between max-w-6xl mx-auto py-4 px-4">
-            <h1 className="w-3/4 font-medium text-3xl text-white">{name}</h1>
+        <div className="bg-indigo-500 w-full flex flex-row justify-between max-w-6xl mx-auto py-4 px-12">
+            <h2 className="font-medium text-3xl text-white">{name}</h2>
             {children}
         </div>   
         );
